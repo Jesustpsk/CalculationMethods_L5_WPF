913a837cfa54a3b4d191a9397036b71851d302c8
Jesustpsk <dm.kuzmin11@mail.ru>
2023-10-19 00:53:08 +0300

    Initial commit

---
+++ Lagrange.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace CalculationMethods_L5_WPF;
+
+public abstract class Lagrange
+{
+    private static double[] _xValues;
+    private static double[] _yValues;
+
+    public static void Fill(double[] x, double[] y)
+    {
+        if (x.Length != y.Length) 
+            throw new ArgumentException("Количество узлов должно быть одинаковым.");
+        _xValues = x;
+        _yValues = y;
+    }
+    /*public static double Interpolate(double x)
+    {
+        // Проверка на выход за границы узлов
+        if (Math.Abs(x) < Math.Abs(_xValues[0]) || Math.Abs(x) > Math.Abs(_xValues[^1]))
+            throw new ArgumentException("Точка x находится вне диапазона узлов.");
+
+        // Найти ближайшие узлы
+        var i = 0;
+        while (i < _xValues.Length && Math.Abs(_xValues[i]) < Math.Abs(x))
+            i++;
+
+        // Линейная интерполяция
+        var x0 = _xValues[i - 1];
+        var x1 = _xValues[i];
+        var y0 = _yValues[i - 1];
+        var y1 = _yValues[i];
+
+        var interpolatedValue = y0 + ((x - x0) / (x1 - x0)) * (y1 - y0);
+        return interpolatedValue;
+    }*/
+    public static double Interpolate(double x)
+    {
+        int i = 0;
+        // Проверка на выход за границы узлов
+        if (x >= 0)
+        {
+            if (x < _xValues[0] || x > _xValues[^1])
+                throw new ArgumentException("Точка x находится вне диапазона узлов.");
+            while (i < _xValues.Length && _xValues[i] < x) // Найти ближайшие узлы
+                i++;
+        }
+        else
+        {
+            if (x > _xValues[0] || x < _xValues[^1])
+                throw new ArgumentException("Точка x находится вне диапазона узлов.");
+            while (i < _xValues.Length && _xValues[i] >= x) // Найти ближайшие узлы
+                i++;
+        }
+
+        // Линейная интерполяция
+        var x0 = _xValues[i - 1];
+        var x1 = _xValues[i];
+        var y0 = _yValues[i - 1];
+        var y1 = _yValues[i];
+
+        double interpolatedValue = y0 + ((x - x0) / (x1 - x0)) * (y1 - y0);
+        return interpolatedValue;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static CalculationMethods_L5_WPF.Lagrange;
+
+namespace CalculationMethods_L5_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Fill(new []
+            {
+                Convert.ToDouble(TbX0.Text.Replace('.',',')),
+                Convert.ToDouble(TbX1.Text.Replace('.',',')), Convert.ToDouble(TbX2.Text.Replace('.',',')), Convert.ToDouble(TbX3.Text.Replace('.',',')),
+                Convert.ToDouble(TbX4.Text.Replace('.',',')), Convert.ToDouble(TbX5.Text.Replace('.',',')),Convert.ToDouble(TbX6.Text.Replace('.',',')),
+                Convert.ToDouble(TbX7.Text.Replace('.',',')),Convert.ToDouble(TbX8.Text.Replace('.',',')),Convert.ToDouble(TbX9.Text.Replace('.',',')),
+                Convert.ToDouble(TbX10.Text.Replace('.',','))
+            }, new []
+            {
+                Convert.ToDouble(TbY0.Text.Replace('.',',')),
+                Convert.ToDouble(TbY1.Text.Replace('.',',')), Convert.ToDouble(TbY2.Text.Replace('.',',')), Convert.ToDouble(TbY3.Text.Replace('.',',')),
+                Convert.ToDouble(TbY4.Text.Replace('.',',')), Convert.ToDouble(TbY5.Text.Replace('.',',')),Convert.ToDouble(TbY6.Text.Replace('.',',')),
+                Convert.ToDouble(TbY7.Text.Replace('.',',')),Convert.ToDouble(TbY8.Text.Replace('.',',')),Convert.ToDouble(TbY9.Text.Replace('.',',')),
+                Convert.ToDouble(TbY10.Text.Replace('.',','))
+            });
+            var x = Convert.ToDouble(TbX.Text.Replace('.', ','));
+            TbOutput.Text = Interpolate(x) + "";
+        }
+    }
+}
